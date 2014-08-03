29a2a66741e8e647ae45b45f3ab4e49b2de42208
Mark-Chang-KKBOX <markchang@kkbox.com>
2014-08-03 20:47:49 +0800

    Init Project.

---
+++ unlock.sikuli/unlock.py
@@ -0,0 +1,13 @@
+mouseMove(Pattern("1406376086718.png").similar(0.95).targetOffset(0,-81))
+
+Settings.MoveMouseDelay = 2
+mouseDown(Button.LEFT)
+
+mouseMove(Pattern("1406988581863.png").similar(0.95).targetOffset(-77,0))
+
+mouseMove(Pattern("1406379115328.png").similar(0.95).targetOffset(0,80))
+
+mouseMove(Pattern("1406379171873.png").similar(0.95).targetOffset(76,0))
+
+mouseUp(Button.LEFT)
+
